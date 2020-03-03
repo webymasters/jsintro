a7378df961301bc6f9740ca799caea3186707c8b
Manpreet Singh <n01397855@nj-00118-017.humber.org>
2020-03-03 13:50:41 -0500

    Doing few js basic excercises

---
+++ js/ex.js
@@ -0,0 +1,48 @@
+
+
+//Excercise 1
+let jobTitle =`Web Developer`
+let gLoc =`Toronto,Canada`
+let aSal =`400k`
+
+
+console.log(` You will be a ${jobTitle} in ${gLoc} making ${aSal} per year`)
+
+
+//Excercise 2
+
+let thisYear = 2020
+let birthYear = 1991
+
+console.log(`Your age in this year is  ${thisYear - birthYear}`)
+
+//Excercise 3
+
+let curAge = 28
+let maxAge = 60
+let amountPerDay = 10
+
+let myUsage=(maxAge-curAge) * (amountPerDay *365)
+console.log(`You will need $${myUsage} to last you until the ripe old age of ${maxAge}`)
+
+
+
+//Excercise 4
+
+let radius = 20
+
+
+console.log(`The circle's circumference is ${2*(22/7)*radius}.`)
+console.log(`The circle's area is ${(22/7)*(radius*radius)}".`)
+
+
+//Excercise 4
+
+let celsius = 20
+
+console.log(`${celsius}°C is ${celsius*1.8}°F.`)
+
+let fah = 36
+console.log(`${fah}°F is ${fah*(5/9)}°C.`)
+
+
